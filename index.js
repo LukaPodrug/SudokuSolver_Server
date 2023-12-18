63c55d4c768b1fbfdd276db340bb3645f345158f
LukaPodrug <lukapodra1@gmail.com>
2023-12-18 23:43:15 +0100

    Test with get files route

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const fs = require('fs')
 
 dotenv.config()
 
@@ -24,6 +25,16 @@ app.use('/attempt', attemptRoute)
 
 app.get('/', (req, res) => {
     res.status(200).send('App recieved get request')
+})
+
+app.get('/files', (req, res) => {
+    fs.readdir('./', (err, files) => {
+        files.forEach(file => {
+            console.log(file);
+        })
+    })
+
+    res.status(200).send('App recieved get reuqest on files')
 })
 
 app.listen(port, () => {
