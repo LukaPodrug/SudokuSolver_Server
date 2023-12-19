d9178cde8bfb0f7d6aa11c2dd49b3fe1becd6fb3
LukaPodrug <lukapodra1@gmail.com>
2023-12-19 17:35:25 +0100

    Add test route for libc

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const { family, GLIBC, MUSL } = require('detect-libc')
+const { version } = require('detect-libc')
 
 dotenv.config()
 
@@ -24,6 +26,38 @@ app.use('/attempt', attemptRoute)
 
 app.get('/', (req, res) => {
     res.status(200).send('App recieved get request')
+})
+
+app.get('/lib', async (req, res) => {
+    switch (await family()) {
+        case GLIBC: 
+            console.log("glibc")
+            res.status(200).send('glibc')
+            break
+        case MUSL:
+            console.log("musl")
+            res.status(200).send('musl')
+            break
+        case null:
+            console.log("lib not found")
+            res.status(200).send('lib not found')
+            break
+    }
+})
+
+app.get('/version', async (req, res) => {
+    const version2 = await version()
+    if (version2) {
+        const [major, minor, patch] = version2.split('.')
+        console.log(major)
+        console.log(minor)
+        console.log(patch)
+        res.status(200).send(version)
+    }
+    else {
+        console.log("version not found")
+        res.status(500).send('version not found')
+    }
 })
 
 app.listen(port, () => {
